bd093fb5d09c720f589c5f00a85b0e9f0beaa029
KarinAAC <karin_alejo@hotmail.com>
2017-04-28 12:57:58 -0500

    subiendo ejercicio

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function clave() {
+  span = document.getElementById("contenedor-clave");
+var aleatorio = Math.round(Math.random()*10);
+span.innerHTML= aleatorio;
+
+}
+/*function confirmacion() {
+  respuesta = document.getElementById("contenedor-respuesta").value;
+    span = document.getElementById("contenedor-clave");
+  if(respuesta == span{
+    alert("no eres un robot");
+  }
+}*/
